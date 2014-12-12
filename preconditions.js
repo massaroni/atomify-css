b0d6858ca4b1b24a9ca16b393f940bed19859d41
Kevin Massaroni <kevinmassaroni@gmail.com>
2014-12-12 06:46:37 -0500

    following the existing type checking convention

--- preconditions.js
+++ preconditions.js
@@ -1,14 +1,13 @@
 'use strict';
 
 var preconditions = require('precondition')
-  , _ = require('underscore')
 
 module.exports = {
   check: function check() {
     var args = [].slice.call(arguments)
     var msg = args.length > 1 ? args[1] : null
 
-    if (_.isString(msg)) {
+    if (typeof msg === 'string') {
       args[1] = 'Atomify-CSS error: ' + msg
     }
 
